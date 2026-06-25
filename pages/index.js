06366b184a3157f713e3c90e9570d8fa3478c989
A.Ortiz <uaodev@gmail.com>
2026-06-25 14:09:20 +0000

    Novo testes com commit --amend

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home(){
 
     return <h1>Ambiente de desenvolvimento UAO Dev</h1>
+           <h2>Aplicação conhecimentos github</h2>
+           <h3>Aplicação comando git --amend </h3> 
+           <p> Novo teste</p>
+           <P>!!!!!!!!</P>
 }
 
 export default Home;
